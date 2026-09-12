8447cb206080215c55116c0a6f9a1e736d16378a
sanramoncodeninjas-git <sanramoncodeninjas@gmail.com>
2026-09-12 11:08:37 -0700

    Update

--- graphingfavcandies.py
+++ graphingfavcandies.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-candy_options=["Kitkat", "Reese's","Snickers","Hershey's Cookies 'n' Cream","M&Ms"] 
+candy_options=["Kitkat", "Reese's","Snickers","Hershey's","M&Ms"] 
 votes=[5,2,1,1,1]
 colors=["#EC2227","#FF5200","#58352F", "#003399", "#187C36"]
 
@@ -10,5 +10,21 @@ bars=ax.bar(candy_options,votes,color=colors,edgecolor="black", width=0.6)
 for bar in bars:
   height=bar.get_height()
   ax.annotate(
-      f"{height}",
-      x=(bar.get_x()+bar.get_width()/2,height),
+   f"{height}",
+   x=(bar.get_x()+bar.get_width()/2,height))
+
+
+plt.show()
+
+
+
+
+
+
+
+mytuple = ("foo","bar")
+
+
+
+
+print(mytuple[0])
